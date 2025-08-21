3c59f708fdad3795c470231f4f9a83780e23848a
Tiago <tiago.polemarxgroup@gmail.com>
2025-08-21 19:31:26 -0300

    properties, Game, ORM

---
+++ src/main/java/br/com/api/gamelist/entities/Game.java
@@ -0,0 +1,27 @@
+package br.com.api.gamelist.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "tb_game")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platform;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+
+}

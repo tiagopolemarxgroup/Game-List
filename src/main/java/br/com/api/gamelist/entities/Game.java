3d721e7dbb17f3e67ae5155a7bf48465bd752197
Tiago <tiago.polemarxgroup@gmail.com>
2025-08-21 19:47:33 -0300

    Game Seed

--- src/main/java/br/com/api/gamelist/entities/Game.java
+++ src/main/java/br/com/api/gamelist/entities/Game.java
@@ -18,9 +18,13 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Lob
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 
